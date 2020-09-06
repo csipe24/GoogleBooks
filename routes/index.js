4cf22ebb3aee5bc46b5265425054942afcbb8261
Chris Sipe <csipe24@gmail.com>
2020-09-05 18:33:41 -0700

    Added Routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,31 @@
 const router = require("express").Router();
 const {Book} = require("../models");
 
-router.post()
+router.post("/books", (req, res) => {
+    Book.create(req.body)
+    .then(book => {
+        res.json(book)
+    })
+    .catch(error => {
+        res.status(401).json(err);
+    });
+});
 
-router.delete()
+router.delete("/books/:id", (req, res)=> {
+    Book.findByIdAndDelete(req.params.id, function(err){
+        if (err) console.log(err);
+        console.log("Book Deleted");
+    });
+});
 
-router.get()
+router.get("/books", (req, res)=>{
+    Book.find()
+    .then(book =>{
+        res.json(book);
+    })
+    .catch(err=>{
+        res.status(401).json(err);
+    });
+});
 
 module.exports = router;
